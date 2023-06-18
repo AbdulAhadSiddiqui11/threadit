5e636288861a0c0c94078e3389c086a352cebef8
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2023-06-18 21:06:30 +0530

    Next/Google Auth & Connecting with SQL DB with Prisma

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,12 @@ import Image from "next/image"
 
 import { logo } from '@/assets/images'
 import { buttonVariants } from "./ui/Button"
+import { getAuthSession } from "@/lib/auth"
 
-const Navbar = () => {
+const Navbar = async () => {
+
+    const session = await getAuthSession();
+
     return (
         <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
             <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -21,7 +25,11 @@ const Navbar = () => {
 
                 {/* Search bar */}
 
-                <Link href='/sign-in' className={buttonVariants()}> Sign In </Link>
+                {session ? (
+                    <Link href='/create-post' className={buttonVariants()}> Create Post </Link>
+                ) : (
+                    <Link href='/sign-in' className={buttonVariants()}> Sign In </Link>
+                )}
             </div>
         </div>
     )
